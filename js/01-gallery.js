a6ca03ab3d7cf0fc03cfc2067bbb494c9028fc59
JakBel <73158151+JakBel@users.noreply.github.com>
2021-09-08 18:07:50 +0200

    Corrected version

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,44 +4,50 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const qs = (selector) => document.querySelector(selector);
-const qsa = (selector) => document.querySelectorAll(selector);
-
 const galleryContainer = qs('.gallery');
-const imagesContainer = galleryItems
-    .map(item =>
-        `<div class="gallery__item"> <a class="gallery__link" href="${item.original}"> <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt=${item.description}"/></a> </div>`)
-    .join('');
-galleryContainer.insertAdjacentHTML('beforeend', imagesContainer);
+console.log(galleryContainer);
 
-const keyClosing = (event, istance) => {
-    if ('Escape' === event.key) {
-        istance.close();
-    }
+const photoContainer = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item"> <a class="gallery__link" href="${item.original}"> <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a> </div>`
+  )
+  .join('');
+galleryContainer.insertAdjacentHTML('beforeend', photoContainer);
+console.log(photoContainer);
+
+const keyClosing = (event, instance) => {
+  if ('Escape' === event.key) {
+    instance.close();
+  }
+  console.log(event.key);
 };
 
-const images = qsa('.gallery__item gallery__image');
+const qsa = (selector) => document.querySelectorAll(selector);
+const images = qsa('.gallery__item .gallery__image');
+console.log(images);
 images.forEach(image => {
-    image.addEventListener('click', event => {
-        event.preventDefault();
+  image.addEventListener('click', event => {
+    event.preventDefault();
 
-        const instance = basicLightbox.create(
-            `<img src="${image.original}">`,
-            {
-                onClose: instance => {
-                    galleryContainer.removeEventListener('keydown', event =>
-                        keyClosing(event, instance));
-                    return true;
-                },
-                onShow: instance => {
-                    galleryContainer.addEventListener('keydown', event =>
-                        keyClosing(event, instance));
-                    return true;
-                },
-            }
-        );
-
-        if (event.target.src === image.preview) {
-            instance.show();
+    basicLightbox
+      .create(
+        `<img width="1400" height="900" src="${image.dataset.source}" loading="lazy" alt="${image.alt}">`,
+        {
+          onClose: instance => {
+            galleryContainer.removeEventListener('keydown', event =>
+              keyClosing(event, instance)
+            );
+            return true;
+          },
+          onShow: instance => {
+            galleryContainer.addEventListener('keydown', event =>
+              keyClosing(event, instance)
+            );
+            return true;
+          },
         }
-    });
+      )
+      .show();
+  });
 });
